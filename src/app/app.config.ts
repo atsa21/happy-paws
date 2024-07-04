9dd1fe01846f41c518b1b21e327310a16a90c245
Anna Sakhaudinova <104850911+atsa21@users.noreply.github.com>
2024-07-04 19:36:45 +0300

    added firebase and deployed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+  ]
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBkMJrk3vacxkDIjAOMQB27-3qVSRte-M4",
+    authDomain: "happy-paws-project.firebaseapp.com",
+    databaseURL: "https://happy-paws-project-default-rtdb.firebaseio.com",
+    projectId: "happy-paws-project",
+    storageBucket: "happy-paws-project.appspot.com",
+    messagingSenderId: "870498226641",
+    appId: "1:870498226641:web:12dc9dec031f354f08d0f0"
+  }
+};
